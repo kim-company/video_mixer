1d55cb0b82357ddc8648bed4945c41a3d7d56749
dmorn <danielmorandini@me.com>
2022-10-18 10:48:51 +0200

    Add out-or-order failing test

--- lib/video_mixer/frame_spec.ex
+++ lib/video_mixer/frame_spec.ex
@@ -4,13 +4,13 @@ defmodule VideoMixer.FrameSpec do
           width: pos_integer(),
           height: pos_integer(),
           pixel_format: atom(),
-          expected_frame_size: integer()
+          accepted_frame_size: integer()
         }
-  defstruct [:reference, :width, :height, :pixel_format, :expected_frame_size]
+  defstruct [:reference, :width, :height, :pixel_format, :accepted_frame_size]
 
   @doc "Returns true if `frame` is compatible with the provided specification."
   @spec compatible?(t(), VideoMixer.Frame.t()) :: boolean()
-  def compatible?(%__MODULE__{expected_frame_size: expected}, %VideoMixer.Frame{size: actual}) do
-    expected == actual
+  def compatible?(%__MODULE__{accepted_frame_size: accepted}, %VideoMixer.Frame{size: actual}) do
+    accepted == actual
   end
 end
---
+++ test/video_mixer/frame_queue_test.exs
@@ -0,0 +1,44 @@
+defmodule VideoMixer.FrameQueueTest do
+  use ExUnit.Case
+
+  alias VideoMixer.Frame
+  alias VideoMixer.FrameQueue, as: Queue
+  alias VideoMixer.FrameSpec, as: Spec
+
+  test "handles frame-spec pairs delivered out-of-order" do
+    queue = Queue.new(0)
+    input = [
+      %Frame{size: 100},
+      %Spec{accepted_frame_size: 100},
+      %Frame{size: 100},
+      %Spec{accepted_frame_size: 200},
+      %Frame{size: 100}, # After spec change.
+      %Frame{size: 200},
+      %Spec{accepted_frame_size: 300},
+      %Frame{size: 300},
+      %Frame{size: 200}, # Should not happen but still we prefer to handle it.
+      %Frame{size: 300},
+      %Frame{size: 400}, # Buffer received before associated specs.
+      %Spec{accepted_frame_size: 400},
+    ]
+    want = [
+      %{frame: %Frame{size: 100}, spec: %Spec{accepted_frame_size: 100}, spec_changed?: true},
+      %{frame: %Frame{size: 100}, spec: %Spec{accepted_frame_size: 100}, spec_changed?: false},
+      %{frame: %Frame{size: 100}, spec: %Spec{accepted_frame_size: 100}, spec_changed?: false},
+      %{frame: %Frame{size: 300}, spec: %Spec{accepted_frame_size: 300}, spec_changed?: true},
+      %{frame: %Frame{size: 200}, spec: %Spec{accepted_frame_size: 200}, spec_changed?: true},
+      %{frame: %Frame{size: 300}, spec: %Spec{accepted_frame_size: 300}, spec_changed?: true},
+      %{frame: %Frame{size: 400}, spec: %Spec{accepted_frame_size: 400}, spec_changed?: true},
+    ]
+
+    # Load the queue
+    queue = Enum.reduce(input, queue, fn frame_or_spec, queue -> Queue.push(queue, frame_or_spec) end)
+
+    # Assert its contents
+    Enum.reduce(want, queue, fn want, queue ->
+      {have, queue} = Queue.pop!(queue)
+      assert ^have = want
+      queue
+    end)
+  end
+end
